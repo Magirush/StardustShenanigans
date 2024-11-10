051d7d737655be9886fc98f64cf9b32bb75cbb55
KazooKat <glibpitt@gmail.com>
2024-11-10 10:51:29 -0500

    added main gui

--- InfusionBoard.py
+++ InfusionBoard.py
@@ -106,11 +106,19 @@ class InfusionBoard:
             return -1 if not eligible else random.choice(eligible)
  
     # Board creator function (random, in-game style)
-    def __init__(self, startingValue: int, planetCount: int, starCount: int):
+    def __init__(self, startingValue: int, planetCount: int, starCount: int, 
+                 useAlternateConstructorLogic: bool = False, alternateConstructorStateMat: np.ndarray = np.zeros((6, 7)), alternateConstructorTurnsMat: np.ndarray = np.zeros((6, 7))):        
         eligible = []
 
         self.board = np.zeros((self.Domains, self.ySize, self.xSize),dtype=int)
+            # Constants for board size
+        xSize = 7
+        ySize = 6
+        Domains = 2
 
+        # Constants for board cost
+        forgeCost = 10
+        turnCost = 1
         # Initialise empty ledgers for turn & tile change
         self.turnChangeLedger = []
         self.tileChangeLedger = []
@@ -125,52 +133,53 @@ class InfusionBoard:
         self.new_plasma = 0
         self.nova_destroyed_by_blackhole = 0
 
-        for x in range(self.xSize):
-            for y in range(self.ySize):
-                eligible.append((x,y))
-        
-        for i in range(planetCount):
-            if len(eligible) != 0:
-                x, y = eligible[random.randrange(len(eligible))]
-                self.board[0, y, x] = 9
-                eligible.remove((x,y))
+        if not useAlternateConstructorLogic:
+            eligible = []
 
-        for i in range(starCount):
-            if len(eligible) != 0:
-                x, y = eligible[random.randrange(len(eligible))]
-                self.board[0, y, x] = 8 if random.random() < 0.66 else 6
-                eligible.remove((x,y))
-        
-        placeItem = self.randomPlacementItem(startingValue)
-        while(placeItem != -1 and len(eligible) > 0):
-            x, y = eligible[random.randrange(len(eligible))]
-            self.board[0, y, x] = placeItem
-            eligible.remove((x,y))
-            startingValue -= self.placementValues[placeItem]
+            self.board = np.zeros((self.Domains, self.ySize, self.xSize),dtype=int)
+
+            for x in range(self.xSize):
+                for y in range(self.ySize):
+                    eligible.append((x,y))
+            
+            for i in range(planetCount):
+                if len(eligible) != 0:
+                    x, y = eligible[random.randrange(len(eligible))]
+                    self.board[0, y, x] = 9
+                    eligible.remove((x,y))
+
+            for i in range(starCount):
+                if len(eligible) != 0:
+                    x, y = eligible[random.randrange(len(eligible))]
+                    self.board[0, y, x] = 8 if random.random() < 0.66 else 6
+                    eligible.remove((x,y))
+            
             placeItem = self.randomPlacementItem(startingValue)
-        
-        eligible.clear()
-        y, x = np.where(np.isin(self.board[0], [0,1,9], invert=True))
-        coords = zip(x,y)
-        for coord in coords:
-            eligible.append(coord)
-        
-        itemTurn = 1
-        while len(eligible) > 0:
-            x, y = eligible[random.randrange(len(eligible))]
-            self.board[1, y, x] = itemTurn
-            itemTurn += 1
-            eligible.remove((x,y))
-        
-        self.boardinitial=np.copy(self.board)
-
-    def alternate_constructor(self, state: np.ndarray, turns: np.ndarray):
-        if type(state) == None or type(turns) == None:
-            self.board=self.__init__(0,0,0)
+            while(placeItem != -1 and len(eligible) > 0):
+                x, y = eligible[random.randrange(len(eligible))]
+                self.board[0, y, x] = placeItem
+                eligible.remove((x,y))
+                startingValue -= self.placementValues[placeItem]
+                placeItem = self.randomPlacementItem(startingValue)
+            
+            eligible.clear()
+            y, x = np.where(np.isin(self.board[0], [0,1,9], invert=True))
+            coords = zip(x,y)
+            for coord in coords:
+                eligible.append(coord)
+            
+            itemTurn = 1
+            while len(eligible) > 0:
+                x, y = eligible[random.randrange(len(eligible))]
+                self.board[1, y, x] = itemTurn
+                itemTurn += 1
+                eligible.remove((x,y))
+            
+            self.boardinitial=np.copy(self.board)
         else:
-            self.board = np.stack((state, turns))
-            self.boardinitial = np.copy(self.board)
-    
+            # Manual mode
+            self.board = np.stack((alternateConstructorStateMat, alternateConstructorTurnsMat))
+            self.boardinitial = np.copy(self.board)    
     # Utils
     def in_bounds(self, x:int,y:int) -> bool:
         # Checks if the x and y values are in-bounds of the board 
--- main.py
+++ main.py
@@ -1,27 +1,180 @@
 from InfusionBoard import InfusionBoard
-import BoardHeuristics
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
+from BoardHeuristics import a_star_search
+
+class StardustInfusionReplicator:
+    '''
+    Class that defines the GUI for our replication of the Stardust Infusion minigame.
+    '''
+    
+    def __init__(self, root):
+        '''Setup GUI Main Window, initialize several variables.'''
+
+        self.root = root
+        self.root.title("Stardust Infusion Entry START")
+        self.root.state('zoomed')
+        self.root.wm_attributes("-topmost", 1)
+
+        # Schedule a function to disable topmost priority.
+        self.root.after(50, self.disable_topmost)
+
+        # Start Page Structure
+        self.notebook = ttk.Notebook(root)
+        self.notebook.pack()
+
+        self.start_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.start_frame, text="Stardust Infusion Entry Start")
+
+        start_button = tk.Button(self.start_frame, text="START", command=self.entry_frame)
+        start_button.pack(pady=20)
+        
+        self.dropdown_options = {
+            "Gas":{"filename":"./gui/gas.png", "number":0, "lock_times":True},
+            "Plasma":{"filename":"./gui/plasma.png", "number":1, "lock_times":True},
+            "Black Hole":{"filename":"./gui/black_hole.png", "number":2, "lock_times":False},
+            "Nova":{"filename":"./gui/nova.png", "number":3, "lock_times":False},
+            "Supernova":{"filename":"./gui/supernova.png", "number":4, "lock_times":False},
+            "Quasar":{"filename":"./gui/quasar.png", "number":5, "lock_times":False},
+            "Pulsar":{"filename":"./gui/pulsar.png", "number":6, "lock_times":False},
+            "Nebula":{"filename":"./gui/nebula.png", "number":7, "lock_times":False},
+            "Star":{"filename":"./gui/star.png", "number":8, "lock_times":False},
+            "Planet":{"filename":"./gui/planet.png", "number":9, "lock_times":True}
+        }
+
+        print(list(self.dropdown_options.keys()))
+
+        # Dummy initial vars...
+        self.root.mainloop()
+
+    def disable_topmost(self):
+        '''Disables topmost window priority.'''
+        self.root.wm_attributes("-topmost", False)
+
+    def entry_frame(self):
+        '''The frame where we enter the initial state of the board.'''
+
+        # Initialize Entry Frame
+        self.entries_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.entries_frame, text="Stardust Infusion Entry")
+
+        self.all_dropdowns = []
+        self.all_entries = []
+
+        for row in range(6):
+            for col in range(7):
+
+                # Combobox
+                dropdown = ttk.Combobox(self.entries_frame, values=list(self.dropdown_options.keys()), width=10)
+                dropdown.grid(row=row, column=col*2, padx=5,pady=5,sticky="w")
+                dropdown.current(0)
+
+                # Entry Field
+                # Default to 0.
+                text = tk.StringVar()
+                text.set("0")
+
+                entry = tk.Entry(self.entries_frame, width=5, textvariable=text)
+                entry.grid(row=row, column=col*2+1, padx=5, pady=5, sticky="e")
+
+                self.all_dropdowns.append(dropdown)
+                self.all_entries.append(entry)
+
+        print(self.all_dropdowns)
+        print(self.all_entries)
+
+        button = tk.Button(self.entries_frame, width = 20, text="Process Input", command=self.process_inputs)
+        button.grid(row=8, column=15, padx=0, pady=20)
+        self.notebook.select(self.entries_frame)
+
+    def result_frame(self,final_board: np.array):
+        '''The frame where we enter the final state of the board.'''
+
+        # Initialize Result Frame
+        self.result_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.result_frame, text="Stardust Infusion Result")
+
+        self.all_dropdowns = []
+        self.all_entries = []
+
+        self.final_board = final_board
+
+        for row in range(6):
+            for col in range(7):
+
+                # Combobox
+                dropdown2 = ttk.Combobox(self.result_frame, values=list(self.dropdown_options.keys()), width=10, state="disabled")
+                dropdown2.grid(row=row, column=col*2, padx=5,pady=5,sticky="w")
+                dropdown2.current(self.final_board[0,row,col])
+
+                # Entry Field
+                # Default to 0.
+                text = tk.StringVar()
+                text.set(str(final_board[1,row,col]))
+
+                entry2 = tk.Entry(self.result_frame, width=5, textvariable=text,state="disabled")
+                entry2.grid(row=row, column=col*2+1, padx=5, pady=5, sticky="e")
+
+
+
+        self.notebook.select(self.result_frame)
+
+
+    def process_inputs(self):
+
+        # Gather all the inputs for the dropdowns
+        state_string = "".join([str(self.dropdown_options[combobox.get()]["number"]) for combobox in self.all_dropdowns])
+        turn_string = "".join([entry.get() for entry in self.all_entries])
+
+        print(state_string)
+        print(turn_string)
+        state_arr = np.array(list(state_string),dtype=int).reshape((6,7))
+        turn_arr = np.array(list(turn_string),dtype=int).reshape((6,7))
+
+        board = InfusionBoard(0,0,0,True, state_arr, turn_arr)
+
+        #print(board)
+        #print("\n\n\n\n\n\n\n\n\n")
+        #print(board.forge_item())
+        
+        #TODO shove the board into the algorithm, recieve a best state, 
+        best_state = a_star_search(board)
+        self.result_frame(best_state[0].board)
+        #print(board.forge_item(best_state))
+        
+
+
+
+
+
+
+
+
+
+
+
+def input_matrix():
+    """Takes matrix input from the command line."""
+
+    rows = int(input("Enter the number of rows (yDim): "))
+    #cols = int(input("Enter the number of columns (xDim): "))
+
+    matrix = []
+    for i in range(rows):
+        row = input(f"Enter the elements of row {i+1} (space-separated): ").split()
+        row = [int(x) for x in row]
+        matrix.append(row)
+
+    return np.array(matrix)
+
 def main():
-    board = InfusionBoard(40,2,2)
-    BoardHeuristics.node_stats(board)
-    
-    board1, cost = BoardHeuristics.a_star_search(board)
-    # print("the best board found")
-    # print(board1)
-    # print(cost)
-    # print("\n")
-    # print(board1.forge_item())
-    # boards = BoardHeuristics.get_moves(board)
-    # for i in boards:
-    #     print(i)
-    #     print(BoardHeuristics.board_value(i))
-    # print(len(boards))
-    # print(BoardHeuristics.board_value(board))
-    # print("The final board initial state is")
-    # print(board1)
-    # print(f"Tile Ledger: {board1.tileChangeLedger}")
-    # print(f"Turn Ledger: {board1.turnChangeLedger}")
-    #BoardHeuristics.node_stats(board1)
-    
+
+    app = tk.Tk()
+    entry_app = StardustInfusionReplicator(app)
+    app.mainloop()
+
+
 
 if __name__ == "__main__":
     main()
